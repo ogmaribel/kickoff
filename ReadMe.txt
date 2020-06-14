09a9545bc99cd7dc1f61faa45d71439080aaa28e
ogmaribel <32981590+ogmaribel@users.noreply.github.com>
2020-06-14 12:09:38 -0500

    first add

---
+++ ReadMe.txt
@@ -0,0 +1,24 @@
+Bootstrap 4 Template For Promoting Startup Products
+
+Theme name:
+=======================================================================
+Velocity (Bootstrap 4)
+
+Theme version:
+=======================================================================
+v4.1.1
+
+Release Date:
+=======================================================================
+20 April 2020
+
+Author: 
+=======================================================================
+Xiaoying Riley at 3rd Wave Media
+
+Contact:
+=======================================================================
+Web: http://themes.3rdwavemedia.com/
+Email: themes@3rdwavemedia.com
+Facebook: https://www.facebook.com/3rdwavethemes/
+Twitter: 3rdwave_themes
---
+++ velocity-bs4/assets/js/main.js
@@ -0,0 +1,61 @@
+/* ======= Fixed header when scrolled ======= */    
+$(window).on('load', function() {
+     
+     if ($(window).scrollTop() > 0) {
+         $('#header').addClass('scrolled');
+     }
+     else {
+         $('#header').removeClass('scrolled');
+         
+     }
+});
+
+
+$(document).ready(function() {
+    
+    /* ======= Fixed header when scrolled ======= */    
+    $(window).on('scroll', function() {
+         
+         if ($(window).scrollTop() > 0) {
+             $('#header').addClass('scrolled');
+         }
+         else {
+             $('#header').removeClass('scrolled');
+             
+         }
+    });
+    
+    /* ======= FAQ accordion ======= */  
+    $('.card-toggle').on('click', function () {
+      if ($(this).find('svg').attr('data-icon') == 'plus-square' ) {
+        $(this).find('svg').attr('data-icon', 'minus-square');
+      } else {
+        $(this).find('svg').attr('data-icon', 'plus-square');
+      };
+    });
+	
+    
+    /* ======= Header Background Slideshow - Flexslider ======= */    
+    /* Ref: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties */
+    
+    $('.bg-slider').flexslider({
+        animation: "fade",
+        directionNav: false, //remove the default direction-nav - https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
+        controlNav: false, //remove the default control-nav
+        slideshowSpeed: 8000
+    });
+	
+	/* ======= Stop Video Playing When Close the Modal Window ====== */
+    $("#modal-video .close").on("click", function() {
+        $("#modal-video iframe").attr("src", $("#modal-video iframe").attr("src"));        
+    });
+     
+    
+     /* ======= Testimonial Bootstrap Carousel ======= */
+     /* Ref: http://getbootstrap.com/javascript/#carousel */
+    $('#testimonials-carousel').carousel({
+      interval: 8000 
+    });
+
+
+});
